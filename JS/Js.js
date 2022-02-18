a2354819227cab91a55645d4fdb6f3ab05eb921e
AnaK11 <99828941+AnaK11@users.noreply.github.com>
2022-02-17 19:26:22 -0500

    Add files via upload

---
+++ JS/Js.js
@@ -0,0 +1,482 @@
+//MAYORES
+
+function CambiarPosicionAngie(){
+  let posicion = document.getElementById('posicionAngie').textContent= 'Delantera'
+}
+
+function CambiarEquipoAngie(){
+  let equipo = document.getElementById('equipoAngie').textContent = 'La Equidad'
+}
+
+function CambiarEdadAngie(){
+  let edad = document.getElementById('edadAngie').textContent = '24 Años'
+}
+
+function CambiarPosicionCarolina(){
+  let posicion = document.getElementById('posicionCarolina').textContent= 'Defensa'
+}
+
+function CambiarEquipoCarolina(){
+  let equipo = document.getElementById('equipoCarolina').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadCarolina(){
+  let edad = document.getElementById('edadCarolina').textContent = '31 Años'
+}
+
+function CambiarPosicionCatalina(){
+  let posicion = document.getElementById('posicionCatalina').textContent= 'Portera'
+}
+
+function CambiarEquipoCatalina(){
+  let equipo = document.getElementById('equipoCatalina').textContent = 'Real Betis'
+}
+
+function CambiarEdadCatalina(){
+  let edad = document.getElementById('edadCatalina').textContent = '27 Años'
+}
+
+function CambiarPosicionDaniela(){
+  let posicion = document.getElementById('posicionDaniela').textContent= 'Defensa'
+}
+
+function CambiarEquipoDaniela(){
+  let equipo = document.getElementById('equipoDaniela').textContent = 'América de Cali'
+}
+
+function CambiarEdadDaniela(){
+  let edad = document.getElementById('edadDaniela').textContent = '27 Años'
+}
+
+function CambiarPosicionDanielaM(){
+  let posicion = document.getElementById('posicionDanielaM').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoDanielaM(){
+  let equipo = document.getElementById('equipoDanielaM').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadDanielaM(){
+  let edad = document.getElementById('edadDanielaM').textContent = '30 Años'
+}
+
+function CambiarPosicionJorelyn(){
+  let posicion = document.getElementById('posicionJorelyn').textContent= 'Defensa'
+}
+
+function CambiarEquipoJorelyn(){
+  let equipo = document.getElementById('equipoJorelyn').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadJorelyn(){
+  let edad = document.getElementById('edadJorelyn').textContent = '24 Años'
+}
+
+function CambiarPosicionLeicy(){
+  let posicion = document.getElementById('posicionLeicy').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoLeicy(){
+  let equipo = document.getElementById('equipoLeicy').textContent = 'Atlético de Madrid'
+}
+
+function CambiarEdadLeicy(){
+  let edad = document.getElementById('edadLeicy').textContent = '25 Años'
+}
+
+function CambiarPosicionLiana(){Andrea
+  let posicion = document.getElementById('posicionLiana').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoLiana(){
+  let equipo = document.getElementById('equipoLiana').textContent = 'Independiente Santa Fe'
+}
+
+function CambiarEdadLiana(){
+  let edad = document.getElementById('edadLiana').textContent = '29 Años'
+}
+
+function CambiarPosicionLinda(){
+  let posicion = document.getElementById('posicionLinda').textContent= 'Delantera'
+}
+
+function CambiarEquipoLinda(){
+  let equipo = document.getElementById('equipoLinda').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadLinda(){
+  let edad = document.getElementById('edadLinda').textContent = '16 Años'
+}
+
+function CambiarPosicionLorena(){
+  let posicion = document.getElementById('posicionLorena').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoLorena(){
+  let equipo = document.getElementById('equipoLorena').textContent = 'Atlético Nacional'
+}
+
+function CambiarEdadLorena(){
+  let edad = document.getElementById('edadLorena').textContent = '24 Años'
+}
+
+function CambiarPosicionManuela(){
+  let posicion = document.getElementById('posicionManuela').textContent= 'Delantera'
+}
+
+function CambiarEquipoManuela(){
+  let equipo = document.getElementById('equipoManuela').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadManuela(){
+  let edad = document.getElementById('edadManuela').textContent = '21 Años'
+}
+
+function CambiarPosicionManuelaV(){
+  let posicion = document.getElementById('posicionManuelaV').textContent= 'Defensa'
+}
+
+function CambiarEquipoManuelaV(){
+  let equipo = document.getElementById('equipoManuelaV').textContent = 'Real Sociedad'
+}
+
+function CambiarEdadManuelaV(){
+  let edad = document.getElementById('edadManuelaV').textContent = '21 Años'
+}
+
+
+function CambiarPosicionMaria(){
+  let posicion = document.getElementById('posicionMaria').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoMaria(){
+  let equipo = document.getElementById('equipoMaria').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadMaria(){
+  let edad = document.getElementById('edadMaria').textContent = '25 Años'
+}
+
+function CambiarPosicionMayra(){
+  let posicion = document.getElementById('posicionMayra').textContent= 'Delantera'
+}
+
+function CambiarEquipoMayra(){
+  let equipo = document.getElementById('equipoMayra').textContent = 'Sporting Club de Huelva'
+}
+
+function CambiarEdadMayra(){
+  let edad = document.getElementById('edadMayra').textContent = '30 Años'
+}
+
+function CambiarPosicionStefany(){
+  let posicion = document.getElementById('posicionStefany').textContent= 'Portera'
+}
+
+function CambiarEquipoStefany(){
+  let equipo = document.getElementById('equipoStefany').textContent = 'Colo-Colo'
+}
+
+function CambiarEdadStefany(){
+  let edad = document.getElementById('edadStefany').textContent = '27 Años'
+}
+
+function CambiarPosicionTatiana(){
+  let posicion = document.getElementById('posicionTatiana').textContent= 'Delantera'
+}
+
+function CambiarEquipoTatiana(){
+  let equipo = document.getElementById('equipoTatiana').textContent = 'Independiente Santa Fe'
+}
+
+function CambiarEdadTatiana(){
+  let edad = document.getElementById('edadTatiana').textContent = '30 Años'
+}
+
+function CambiarPosicionViviana(){
+  let posicion = document.getElementById('posicionViviana').textContent= 'Defensa'
+}
+
+function CambiarEquipoViviana(){
+  let equipo = document.getElementById('equipoViviana').textContent = 'Independiente Santa Fe'
+}
+
+function CambiarEdadViviana(){
+  let edad = document.getElementById('edadViviana').textContent = '23 Años'
+}
+
+//JUVENIL
+
+function CambiarPosicionAnaG(){
+  let posicion = document.getElementById('posicionAnaG').textContent= 'Defensa'
+}
+
+function CambiarEquipoAnaG(){
+  let equipo = document.getElementById('equipoAnaG').textContent = 'Atlético Dos Quebradas (Liga Risaralda)'
+}
+
+function CambiarEdadAnaG(){
+  let edad = document.getElementById('edadAnaG').textContent = '16 Años'
+}
+
+function CambiarPosicionAnaM(){
+  let posicion = document.getElementById('posicionAnaM').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoAnaM(){
+  let equipo = document.getElementById('equipoAnaM').textContent = 'Club Andrés Escobar (Liga Risaralda)'
+}
+
+function CambiarEdadAnaM(){
+  let edad = document.getElementById('edadAnaM').textContent = '16 Años'
+}
+
+function CambiarPosicionAngie(){
+  let posicion = document.getElementById('posicionAngie').textContent= ' Defensa'
+}
+
+function CambiarEquipoAngie(){
+  let equipo = document.getElementById('equipoAngie').textContent = 'Atlético Nacional'
+}
+
+function CambiarEdadAngie(){
+  let edad = document.getElementById('edadAngie').textContent = '15 Años'
+}
+
+function CambiarPosicionEliana(){
+  let posicion = document.getElementById('posicionEliana').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoEliana(){
+  let equipo = document.getElementById('equipoEliana').textContent = 'Mochi Futbol Club (Liga Risaralda)'
+}
+
+function CambiarEdadEliana(){
+  let edad = document.getElementById('edadEliana').textContent = '15 Años'
+}
+
+function CambiarPosicionElsa(){
+  let posicion = document.getElementById('posicionElsa').textContent= 'Defensa'
+}
+
+function CambiarEquipoElsa(){
+  let equipo = document.getElementById('equipoElsa').textContent = 'Atlético Dos Quebradas (Liga Risaralda)'
+}
+
+function CambiarEdadElsa(){
+  let edad = document.getElementById('edadElsa').textContent = '15 Años'
+}
+
+function CambiarPosicionGabriela(){
+  let posicion = document.getElementById('posicionGabriela').textContent= 'Delantera'
+}
+
+function CambiarEquipoGabriela(){
+  let equipo = document.getElementById('equipoGabriela').textContent = 'America de Cali'
+}
+
+function CambiarEdadGabriela(){
+  let edad = document.getElementById('edadGabriela').textContent = '16 Años'
+}
+
+function CambiarPosicionJimena(){
+  let posicion = document.getElementById('posicionJimena').textContent= 'Portera'
+}
+
+function CambiarEquipoJimena(){
+  let equipo = document.getElementById('equipoJimena').textContent = 'Escuela Carlos Sarmiento (Liga Valle)'
+}
+
+function CambiarEdadJimena(){
+  let edad = document.getElementById('edadJimena').textContent = '15 Años'
+}
+
+function CambiarPosicionJuana(){
+  let posicion = document.getElementById('posicionJuana').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoJuana(){
+  let equipo = document.getElementById('equipoJuana').textContent = 'Escuela Carlos Sarmiento (Liga Valle)'
+}
+
+function CambiarEdadJuana(){
+  let edad = document.getElementById('edadJuana').textContent = '15 Años'
+}
+
+function CambiarPosicionJuanita(){
+  let posicion = document.getElementById('posicionJuanita').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoJuanita(){
+  let equipo = document.getElementById('equipoJuanita').textContent = 'Atlético Nacional'
+}
+
+function CambiarEdadJuanita(){
+  let edad = document.getElementById('edadJuanita').textContent = '15 Años'
+}
+function CambiarPosicionKarlaV(){
+  let posicion = document.getElementById('posicionKarlaV').textContent= 'Delantera'
+}
+
+function CambiarEquipoKarlaV(){
+  let equipo = document.getElementById('equipoKarlaV').textContent = 'Club Besser (Liga Bogotá)'
+}
+
+function CambiarEdadKarlaV(){
+  let edad = document.getElementById('edadKarlaV').textContent = '16 Años'
+}
+function CambiarPosicionKarlaT(){
+  let posicion = document.getElementById('posicionKarlaT').textContent= 'Delantera'
+}
+
+function CambiarEquipoKarlaT(){
+  let equipo = document.getElementById('equipoKarlaT').textContent = 'Generaciones Palmiranas (Liga Valle)'
+}
+
+function CambiarEdadKarlaT(){
+  let edad = document.getElementById('edadKarlaT').textContent = '15 Años'
+}
+
+function CambiarPosicionLinda(){
+  let posicion = document.getElementById('posicionLinda').textContent= 'Delantera'
+}
+
+function CambiarEquipoLinda(){
+  let equipo = document.getElementById('equipoLinda').textContent = 'Deportivo Cali'
+}
+
+function CambiarEdadLinda(){
+  let edad = document.getElementById('edadLinda').textContent = '16 Años'
+}
+
+function CambiarPosicion(){
+  let posicion = document.getElementById('posicion').textContent= 'Portera'
+}
+
+function CambiarEquipo(){
+  let equipo = document.getElementById('equipo').textContent = 'Club Palmira (Liga del Valle)'
+}
+
+function CambiarEdadLuisa(){
+  let edad = document.getElementById('edad').textContent = '14 Años'
+}
+
+function CambiarPosicionMariaC(){
+  let posicion = document.getElementById('posicionMariaC').textContent= 'Defensa'
+}
+
+function CambiarEquipoMariaC(){
+  let equipo = document.getElementById('equipoMariaC').textContent = 'Atlético Dos Quebradas (Liga Risaralda)'
+}
+
+function CambiarEdadMariaC(){
+  let edad = document.getElementById('edadMariaC').textContent = '17 Años'
+}
+
+function CambiarPosicionMariaF(){
+  let posicion = document.getElementById('posicionMariaF').textContent= 'Defensa'
+}
+
+function CambiarEquipoMariaF(){
+  let equipo = document.getElementById('equipoMariaF').textContent = 'Generaciones Palmiranas (Liga Valle)'
+}
+
+function CambiarEdadMariaF(){
+  let edad = document.getElementById('edadMariaF').textContent = '15 Años'
+}
+
+function CambiarPosicionMartina(){
+  let posicion = document.getElementById('posicionMartina').textContent= 'Portera'
+}
+
+function CambiarEquipoMartina(){
+  let equipo = document.getElementById('equipoMartina').textContent = 'Club Besser (Liga Bogotá)'
+}
+
+function CambiarEdadMartina(){
+  let edad = document.getElementById('edadMartina').textContent = '15 Años'
+}
+
+function CambiarPosicionMary(){
+  let posicion = document.getElementById('posicionMary').textContent= 'Defensa'
+}
+
+function CambiarEquipoMary(){
+  let equipo = document.getElementById('equipoMary').textContent = 'C. D. Formas Íntimas (Liga Antioquia)'
+}
+
+function CambiarEdadMary(){
+  let edad = document.getElementById('edadMary').textContent = '16 Años'
+}
+
+function CambiarPosicionOrianna(){
+  let posicion = document.getElementById('posicionOrianna').textContent= 'Delantera'
+}
+
+function CambiarEquipoOrianna(){
+  let equipo = document.getElementById('equipoOrianna').textContent = 'Atlas CP (Liga Valle)'
+}
+
+function CambiarEdadOrianna(){
+  let edad = document.getElementById('edadOrianna').textContent = '16 Años'
+}
+
+function CambiarPosicionPaula(){
+  let posicion = document.getElementById('posicionPaula').textContent= 'Delantera'
+}
+
+function CambiarEquipoPaula(){
+  let equipo = document.getElementById('equipoPaula').textContent = 'Club Besser (Liga Bogotá)'
+}
+
+function CambiarEdadPaula(){
+  let edad = document.getElementById('edadPaula').textContent = '15 Años'
+}
+
+function CambiarPosicionSintia(){
+  let posicion = document.getElementById('posicionSintia').textContent= 'Defensa'
+}
+
+function CambiarEquipoSintia(){
+  let equipo = document.getElementById('equipoSintia').textContent = 'Escuela Carlos Sarmiento (Liga Valle)'
+}
+
+function CambiarEdadSintia(){
+  let edad = document.getElementById('edadSintia').textContent = '15 Años'
+}
+
+function CambiarPosicionSofia(){
+  let posicion = document.getElementById('posicionSofia').textContent= 'Centrocampista'
+}
+
+function CambiarEquipoSofia(){
+  let equipo = document.getElementById('equipoSofia').textContent = 'Atlético Dos Quebradas (Liga Risaralda)'
+}
+
+function CambiarEdadSofia(){
+  let edad = document.getElementById('edadSofia').textContent = '16 Años'
+}
+
+function CambiarPosicionStefania(){
+  let posicion = document.getElementById('posicionStefania').textContent= 'Defensa'
+}
+
+function CambiarEquipoStefania(){
+  let equipo = document.getElementById('equipoStefania').textContent = 'Independiente de Fútbol (Liga Valle)'
+}
+
+function CambiarEdadStefania(){
+  let edad = document.getElementById('edadStefania').textContent = '16 Años'
+}
+
+function CambiarPosicionYesica(){
+  let posicion = document.getElementById('posicionYesica').textContent= 'Delantera'
+}
+
+function CambiarEquipoYesica(){
+  let equipo = document.getElementById('equipoYesica').textContent = 'C. D. Formas Íntimas (Liga Antioquia)'
+}
+
+function CambiarEdadYesica(){
+  let edad = document.getElementById('edadYesica').textContent = '15 Años'
+}
